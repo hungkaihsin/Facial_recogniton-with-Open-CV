f7b04b7ed2a8b74badd524befe2bffe3e1b6ec61
Your Name <you@example.com>
2024-03-25 15:08:40 +0800

    add GUI

--- detector.py
+++ detector.py
@@ -3,29 +3,71 @@ import matplotlib.pyplot as plt
 import tkinter as tk
 from tkinter import ttk
 from tkinter.constants import CENTER
+from tkinter import filedialog
 
 # GUI
 # root menu
 
 window = tk.Tk()
-window.title('GUI')
-window.geometry('455x140')
+window.title('Facial Recognition')
+window.geometry('455x90')
 window.resizable(False, False)
 window.iconbitmap('icon.ico')
 
 
 # Entry
 
-load_file = ttk.Entry(width=55)
-load_file.place(x=40, y=0)
+Entry_load_file = ttk.Entry(width=55)
+Entry_load_file.place(x=40, y=1)
 
 
 
-# buttom
+
 
 # Label
 lb_file_path = ttk.Label(text='Route', background='gray', foreground='white')
-lb_file_path.place(x=0, y=0)
+lb_file_path.place(x=0, y=1)
+
+
+# function
+
+def load_file():
+    if Entry_load_file.get() is None:
+        file_path = filedialog.askopenfilename(filetypes=(('jpg files', '*.jpg'), ('all files', '*.*')))
+        Entry_load_file.insert(0, file_path)
+    else:
+        file_path = filedialog.askopenfilename(filetypes=(('jpg files', '*.jpg'), ('all files', '*.*')))
+        Entry_load_file.delete(0, 'end')
+        Entry_load_file.insert(0, file_path)
+
+def detector():
+    image_path = Entry_load_file.get()
+    image = cv2.imread(image_path)
+    grey_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+
+    face_classifier = cv2.CascadeClassifier(
+    cv2.data.haarcascades + 'haarcascade_frontalface_default.xml'
+    )
+
+    face = face_classifier.detectMultiScale(
+    grey_image, scaleFactor = 1.1, minNeighbors = 5, minSize = (40, 40)
+    )
+
+    for (x, y, w, h) in face:
+        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    plt.figure(figsize = (5, 5))
+    plt.imshow(rgb_image)
+    plt.axis('off')
+    plt.show()
+
+# buttom
+Btn_select_file = ttk.Button(text='Select file', command= load_file)
+Btn_select_file.place(x=380, y=0)
+Btn_detect = ttk.Button(text="Detect", command=detector)
+Btn_detect.place(x=230, y=60, anchor= CENTER)
 
 
 window.mainloop()
@@ -36,59 +78,3 @@ window.mainloop()
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# facial recognition function
-
-'''
-image_path = 'input_image.jpg'
-image = cv2.imread(image_path)
-
-
-
-# in order to elavate the efficency, turn image to grayscale
-
-grey_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-'''
-'''
-print(grey_image.shape)
-
-'''
-
-
-'''
-
-# load classifier in cv2
-face_classifier = cv2.CascadeClassifier(
-    cv2.data.haarcascades + 'haarcascade_frontalface_default.xml'
-)
-
-face = face_classifier.detectMultiScale(
-    grey_image, scaleFactor = 1.1, minNeighbors = 5, minSize = (40, 40)
-)
-
-for (x, y, w, h) in face:
-    cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 4)
-
-rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-
-plt.figure(figsize = (20, 10))
-plt.imshow(rgb_image)
-plt.axis('off')
-plt.show()
-
-'''
